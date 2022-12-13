b35fbccde80de297d1360f49cf21fce7ebb9f9e0
HernanC95 <cavalieri.h95@gmail.com>
2022-12-13 14:11:50 -0300

    Create OrderCar

--- src/screen/OrderCar.js
+++ src/screen/OrderCar.js
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text ,StyleSheet,Dimensions} from 'react-native'
 import React from 'react'
-
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
 
 export default function OrderCar() {
   return (
-    <View>
-      <Text>OrderCar</Text>
-    </View>
+    <View style={styles.carrusel}>
+    <Text style={styles.titulo1}>Your Model 3</Text>
+  </View>
   )
 }
+const styles = StyleSheet.create({
+    carrusel: {
+      flex: 1,
+      width: windowWidth,
+      display: "flex",
+      justifyContent: "flex-start",
+      alignItems: "center",
+      flexGrow: 1,
+    },
+    titulo1: {
+      marginTop: 60,
+      fontSize: 40,
+      fontWeight: "800",
+    },
+  });
+  
